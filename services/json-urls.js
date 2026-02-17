198b6f2895475a0acca2e1ff12bbe8840138bf66
Mazen Sokar <mazensokar.priv@gmail.com>
2026-02-17 20:59:30 +0200

    Add service and model files for users, products, orders, cart, and wishlist

---
+++ services/json-urls.js
@@ -0,0 +1,3 @@
+export let  usersLink = "https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/users.json"
+export let  productsLink = "https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/products.json"
+export let  ordersLink = "https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/orders.json"
