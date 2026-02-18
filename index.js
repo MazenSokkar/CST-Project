5858fb1df8cd34f70e733a803b1e411602790f1e
Eman <genedyeman6@gmail.com>
2026-02-18 17:53:46 +0200

    Products, Add Product and Delete Product

---
+++ index.js
@@ -0,0 +1,39 @@
+//Test --> Get All Products
+
+// import { getAllProducts } from './services/product.service.js';
+// getAllProducts().then(products => {
+//     console.log(products);
+// });
+
+//---------------------------------------------------------------------------
+
+// Test --> Add New Product
+
+// import { getAllProducts, addProduct } from './services/product.service.js';
+// getAllProducts().then(products => {
+//     console.log(products);
+// });
+// let newProduct = {
+//     "Category": "Electronics",
+//     "Color": ["Black"],
+//     "Description": "New product",
+//     "Discount": 5,
+//     "Name": "Test Product",
+//     "Price": 1000,
+//     "Quantity": 10,
+//     "Rate": 4.5,
+//     "SellerName": "Tech World",
+//     "imageUrl": ["images/test.jpg"],
+//     "isBestSeller": false,
+//     "isFeatured": false
+// };
+
+// addProduct(newProduct)
+//     .then(() => console.log("Product added successfully"))
+//     .catch(err => console.error("Error adding product:", err));
+
+//---------------------------------------------------------------------------
+
+// Delete Product
+// import {deleteProduct } from './services/product.service.js';
+// deleteProduct(101);
--- services/product.service.js
+++ services/product.service.js
@@ -3,3 +3,84 @@
 // add new
 // update
 // delete
+
+import { Product } from '../shared/models/product.model.js';
+
+// get all products 
+export async function getAllProducts() {
+
+    let products = [];  
+    try {
+        const response = await fetch("https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/products.json");
+        const data = await response.json();
+
+        if (data) {
+
+            const values = Object.values(data);
+
+            values.forEach(product => {
+
+                const newProduct = new Product(
+                    product.Id,
+                    product.Name,
+                    product.Description,
+                    product.Price,
+                    product.Category,
+                    product.Color,
+                    product.Quantity,
+                    product.Discount,
+                    product.Rate,
+                    product.SellerName,
+                    product.imageUrl,
+                    product.isBestSeller,
+                    product.isFeatured,
+                    product.CreatedAt ?? null,
+                    product.Size ?? null
+                );
+
+                products.push(newProduct);   // بضيف العناصر فى ال array
+            });
+        }
+
+    } catch (error) {
+        console.error("Error fetching products:", error);
+    }
+
+    return products;   
+}
+
+// Add New Product
+export async function addProduct(product) {
+    try {
+        const products = await getAllProducts();
+
+        let lastId = 0;
+        if (products.length > 0) {
+            lastId = products[products.length - 1].Id;
+        }
+
+        product.Id = lastId + 1;
+
+        return fetch(`https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/products/${product.Id}.json`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(product)
+        });
+
+    } catch (error) {
+        console.error('Error adding product:', error);
+    }
+}
+
+//Delete Product
+export async function deleteProduct(id) {
+    return fetch(`https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`, {
+        method: 'DELETE'
+    })
+    .then(() => console.log("Product deleted successfully"))
+    .catch(error => console.error('Error deleting product:', error));
+}
+
+
+
+
