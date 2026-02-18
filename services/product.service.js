b3bcf25cfedb83047bd720bddcc9d0887b27e812
Ziad Gad <ziadmohamedgad@gmail.com>
2026-02-18 17:14:36 +0200

    GetProductByID function implemented

--- services/product.service.js
+++ services/product.service.js
@@ -3,3 +3,35 @@
 // add new
 // update
 // delete
+//      
+
+// Get Product By ID
+import { Product } from "../shared/models/product.model.js";
+export async function GetProductById(id) {
+    const response = await fetch(`https://ecommerce-database-dcfc2-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`);
+    if (!response.ok) 
+        throw new Error(`Failed to fetch product with id: ${id}`); 
+    const ProductData = await response.json();
+    if (!ProductData) {
+        throw new Error(`Product with id ${id} not found`);
+    }
+    return new Product(
+        ProductData.Id,
+        ProductData.Name,
+        ProductData.Description,
+        ProductData.Price,
+        ProductData.Category,
+        ProductData.Color,
+        ProductData.Quantity,
+        ProductData.Discount,
+        ProductData.Rate,
+        ProductData.SellerName,
+        ProductData.ImageUrl,
+        ProductData.IsBestSeller,
+        ProductData.IsFeatured,
+        ProductData.CreatedAt,
+        ProductData.Size
+    );
+
+
+}
