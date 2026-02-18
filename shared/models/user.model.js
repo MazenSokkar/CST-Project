7626ba87894dc0fc04d517906d3c534bfb405c1f
Mazen Sokar <mazensokar.priv@gmail.com>
2026-02-18 15:25:18 +0200

    Refactor model classes to use PascalCase for properties in Cart, Order, Product, User, and Wishlist

--- shared/models/user.model.js
+++ shared/models/user.model.js
@@ -1,53 +1,11 @@
 export class User {
     constructor(id, name, username, password, role, address, createdAt) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.address = address;
-        this.createdAt = createdAt;
-    }
-    get id() {
-        return this._id;
-    }
-    set id(value) {
-        this._id = value;
-    }
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
-    get username() {
-        return this._username;
-    }
-    set username(value) {
-        this._username = value;
-    }   
-    get password() {
-        return this._password;
-    }
-    set password(value) {
-        this._password = value;
-    }
-    get role() {
-        return this._role;
-    }   
-    set role(value) {
-        this._role = value;
-    }
-    get address() {
-        return this._address;
-    }   
-    set address(value) {    
-        this._address = value;
-    }
-    get createdAt() {
-        return this._createdAt;
-    }
-    set createdAt(value) {
-        this._createdAt = value;
+        this.Id = id;
+        this.Name = name;
+        this.Username = username;
+        this.Password = password;
+        this.Role = role;
+        this.Address = address;
+        this.CreatedAt = createdAt;
     }
 }
--- shared/models/wishlist.model.js
+++ shared/models/wishlist.model.js
@@ -1,18 +1,6 @@
 export class Wishlist {
     constructor(userId, items) {
-        this.userId = userId;
-        this.items = items;
-    }
-    get userId() {
-        return this._userId;
-    }
-    set userId(value) {
-        this._userId = value;
-    }
-    get items() {
-        return this._items;
-    }
-    set items(value) {
-        this._items = value;
+        this.UserId = userId;
+        this.Items = items;
     }
 }
