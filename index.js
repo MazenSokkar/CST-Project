be0b4b65eefa486222d60e726b344a160ca480b6
Esraa Shiref <israashiref@gmail.com>
2026-02-20 15:31:24 +0200

    Complete navbar and footer structure

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// index.js - works for any page
+// Load Navbar
+fetch('/Shared/Navbar/navbar.html')   // absolute path from root
+  .then(res => res.text())
+  .then(html => document.getElementById('navbar-container').innerHTML = html)
+  .catch(err => console.error("Error loading Navbar:", err));
 
-
-
-
-
+// Load Footer
+fetch('/Shared/Footer/footer.html')   // absolute path from root
+  .then(res => res.text())
+  .then(html => document.getElementById('footer-container').innerHTML = html)
+  .catch(err => console.error("Error loading Footer:", err));
