0539889c3052a255496e56b64b411c5526b97bdf
Mazen Sokar <mazensokar.priv@gmail.com>
2026-02-21 15:56:04 +0200

    Add random image source change on product card hover

--- shared/product-card/product-card.js
+++ shared/product-card/product-card.js
@@ -3,9 +3,17 @@ let productCardSpans = document.querySelectorAll('.product-card-img > span');
 
 productCards.forEach(card => {
     card.addEventListener('mouseenter', () => {
+        card.children[0].children[2].src = `assets/images/${getRandomInt(1, 6)}.png`;
         productCardSpans.forEach(span => span.style.display = 'block');
     });
     card.addEventListener('mouseleave', () => {
+        card.children[0].children[2].src = `assets/images/${getRandomInt(1, 6)}.png`;
         productCardSpans.forEach(span => span.style.display = 'none');
     });
 });
+
+// helpers
+//get random number between min and max
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
